b28b51acfa186f01c608cd1d497985fe98e12298
Arjan <a.kuiper@provrex.nl>
2018-05-09 11:52:51 +0200

    CSRF token in de header op alle paginas

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,6 +3,7 @@
 <title>Aareon | Smart Building</title>
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <link rel="manifest" href="site.webmanifest">
 <link rel="apple-touch-icon" href="icon.png">
