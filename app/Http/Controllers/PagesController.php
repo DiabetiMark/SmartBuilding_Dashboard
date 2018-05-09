a5cf04880ef16dd1e8ded4ac821c883865d6e1e4
Chiel Timmermans <chiel1997@hotmail.com>
2018-05-09 10:34:30 +0200

    Security for password update

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 use Redirect;
 
@@ -10,6 +12,14 @@ class PagesController extends Controller
 {
     public function resetPassword($user_id, $email, $hash)
     {
-        return view('pages.passwordReset', ['user_id' => $user_id, 'email' => $email, 'hash' => $hash]);
+        $count = DB::table('password_resets')
+        ->where('email', $email)
+        ->where('hash', $hash)
+        ->where('user_id', $user_id)
+        ->where('created_at', '>=', Carbon::now()->subMinutes(20))->count();
+        if($count){
+            return view('pages.passwordReset', ['user_id' => $user_id, 'email' => $email, 'hash' => $hash]);
+        }
+        return redirect('/login');
     }
 }
